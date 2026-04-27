a5a358d59c4a32c0759eb969843729b45e386ab6
Ahmed Elgendy <ahmedellgendy@outlook.com>
2026-04-27 14:27:31 +0300

    Refactor Product Entity and Update Database

--- Salesync.Domain/Entities/Product.cs
+++ Salesync.Domain/Entities/Product.cs
@@ -5,11 +5,9 @@
         public required string Name { get; set; }
         public required string Description { get; set; }
         public required string Barcode { get; set; }
-        public required string Type { get; set; }
         public decimal UnitPrice { get; set; }
         public decimal CostPrice { get; set; }  
-        public int WeightInML { get; set; }
-
+        public int StockQuantity { get; set; }
 
     }
 }
