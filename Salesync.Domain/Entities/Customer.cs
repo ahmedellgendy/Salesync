b7649c387d618f9290b7ad2a20282ceb758b8563
Ahmed Elgendy <ahmedellgendy@outlook.com>
2026-04-28 11:08:43 +0300

    Refactor Customer Entity

--- Salesync.Domain/Entities/Customer.cs
+++ Salesync.Domain/Entities/Customer.cs
@@ -2,13 +2,53 @@
 {
     public class Customer : BaseEntity
     {
+        // Basic informations
         public required string Name { get; set; }
-        public required string Type { get; set; }
         public required string Phone { get; set; }
-        public required string Address { get; set; }
-        public int RouteId { get; set; }
+        public required string Email { get; set; }
 
-        public decimal CreditBalance { get; set; }
-        public bool HasDebt { get; set; }
+
+        // Customer Types
+        public CustomerType Type { get; set; }
+        public string? CompanyName { get; set; }
+
+        // Address Information
+        public required string Address { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? PostalCode { get; set; }
+        public string? Country { get; set; }
+
+        // Business Information
+        public decimal CreditLimit { get; set; } 
+        public decimal CurrentBalance { get; set; } 
+        public CustomerStatus Status { get; set; } = CustomerStatus.Active;
+
+
+        // Tracking
+        public DateTime? LastPurchaseDate { get; set; }
+        public decimal TotalPurchaseAmount { get; set; }
+        public int TotalOrders { get; set; }
+
+
+        // Relations
+        public int? BranchId { get; set; }
+        public Branch? Branch { get; set; }
+
+
+
+        public enum CustomerType
+        {
+            Individual = 1,
+            Corporate = 2
+        }
+
+        public enum CustomerStatus
+        {
+            Active = 1,
+            Inactive = 2,
+            Blocked = 3,
+            Pending = 4,
+        }
     }
 }
