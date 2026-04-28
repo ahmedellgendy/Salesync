7cfcccaca56cf0081fc4b5717ce1bb755a143a15
Ahmed Elgendy <ahmedellgendy@outlook.com>
2026-04-28 11:56:30 +0300

    Create ProductDto

---
+++ Salesync.Application/Dtos/ProductDto/ProductDto.cs
@@ -0,0 +1,19 @@
+﻿namespace Salesync.Application.Dtos.ProductDto
+{
+    public class ProductDto
+    {
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required string SKU { get; set; }
+        public required string Barcode { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal CostPrice { get; set; }
+        public decimal? DiscountPercentage { get; set; }
+        public int StockQuantity { get; set; }
+        public int MinStockLevel { get; set; }
+        public int MaxStockLevel { get; set; }
+        public required string Unit { get; set; }
+        public int? WarehouseId { get; set; }
+
+    }
+}
