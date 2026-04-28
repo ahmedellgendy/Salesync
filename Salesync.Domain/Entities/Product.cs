0db22cc35a6df7061d75b821355bb382615a8c9b
Ahmed Elgendy <ahmedellgendy@outlook.com>
2026-04-28 11:13:10 +0300

    Refactor Product Entity

--- Salesync.Domain/Entities/Product.cs
+++ Salesync.Domain/Entities/Product.cs
@@ -2,12 +2,25 @@
 {
     public class Product : BaseEntity
     {
+        // Basic Info
         public required string Name { get; set; }
         public required string Description { get; set; }
         public required string Barcode { get; set; }
+        
+        // Price Info
         public decimal UnitPrice { get; set; }
-        public decimal CostPrice { get; set; }  
+        public decimal CostPrice { get; set; }
+        public decimal? DiscountPercentage { get; set; }
+
+        // Stock Management
         public int StockQuantity { get; set; }
+        public int MinStockLevel { get; set; } 
+        public int MaxStockLevel { get; set; } 
+        public required string Unit { get; set; }
+
+        // Relations
+        public int? WarehouseId { get; set; }
+        public Warehouse? Warehouse { get; set; }
 
     }
 }
