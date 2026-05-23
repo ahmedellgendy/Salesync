5548104470394d84da0a5729aedc67e8406c0f91
Ahmed Elgendy <ahmedellgendy@outlook.com>
2026-05-23 14:15:03 +0300

    Update Customer Module (AddNewFields)

--- Salesync.Infrastructure/Configurations/MasterData/CustomerConfiguration.cs
+++ Salesync.Infrastructure/Configurations/MasterData/CustomerConfiguration.cs
@@ -13,55 +13,76 @@ namespace Salesync.Infrastructure.Configurations.MasterData
 
             builder.Property(c => c.Id)
                 .UseIdentityColumn();
-
             builder.Property(c => c.Name)
                 .IsRequired()
                 .HasMaxLength(100);
-
             builder.Property(c => c.Phone)
-                .IsRequired()
                 .HasMaxLength(15);
-
             builder.Property(c => c.Email)
-                .IsRequired()
                 .HasMaxLength(100);
 
-            builder.Property(c => c.Type)
-                .IsRequired();
-
-            builder.Property(c => c.CompanyName)
-                .HasMaxLength(100);
-
+            builder.Property(c => c.Country)
+                .HasMaxLength(50);
             builder.Property(c => c.Address)
-                .IsRequired()
                 .HasMaxLength(200);
+            builder.Property(c => c.Area)
+                .HasMaxLength(50);
+            builder.Property(c => c.City)
+                .HasMaxLength(50);
+            builder.Property(c => c.District)
+                .HasMaxLength(50);
+            builder.Property(c => c.Region)
+                .HasMaxLength(50);
+            builder.Property(c => c.PostalCode)
+                .HasMaxLength(20);
 
-            builder.Property(c => c.Status)
+            builder.Property(c => c.Latitude)
+                .HasPrecision(18, 8);
+            builder.Property(c => c.Longitude)
+                .HasPrecision(18, 8);
+
+            builder.Property(c => c.CategoryCode)
+                .HasMaxLength(20);
+            builder.Property(c => c.SalesSectorCode)
+                .HasMaxLength(20);
+            builder.Property(c => c.ClassId)
+                .HasMaxLength(20);
+            builder.Property(c => c.Type)
                 .IsRequired();
 
             builder.Property(c => c.CreditLimit)
                 .HasPrecision(18, 2)
                 .HasDefaultValue(0);
-
             builder.Property(c => c.CurrentBalance)
                 .HasPrecision(18, 2)
                 .HasDefaultValue(0);
-
+            builder.Property(c => c.OrderCeiling)
+               .HasPrecision(18, 2);
             builder.Property(c => c.TotalPurchaseAmount)
                 .HasPrecision(18, 2)
                 .HasDefaultValue(0);
 
+            builder.Property(c => c.Status)
+                .IsRequired();
             builder.Property(c => c.CreatedAt)
                 .IsRequired();
-
             builder.Property(c => c.IsActive)
-                .IsRequired()
                 .HasDefaultValue(true);
 
+
+            builder.HasOne(c => c.Branch)
+               .WithMany()
+               .HasForeignKey(c => c.BranchId)
+               .OnDelete(DeleteBehavior.Restrict);
+
             // Indexes
-            builder.HasIndex(c => c.Email).IsUnique();
-            builder.HasIndex(c => c.Phone);
             builder.HasIndex(c => c.Name);
+            builder.HasIndex(c => c.Phone);
+            builder.HasIndex(c => c.Email).IsUnique();
+            builder.HasIndex(c => c.AccountNumber);
+            builder.HasIndex(c => c.TaxId);
+
+           
         }
     }
 }
